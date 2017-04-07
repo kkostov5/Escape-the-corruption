27c2a155a3a0cb26fe31a93da42eb5c45984353e
Krasimir <k_kostov95@abv.bg>
2017-04-07 18:11:03 +0100

    Character is fully controllable.

---
+++ Game-Escape/Assets/Player/Robot/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public GameObject character;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - character.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = character.transform.position + offset;
+	}
+}
---
+++ Game-Escape/Assets/Player/Robot/Scripts/SprintControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprintControl : MonoBehaviour {
+
+	public float topSpeed = 10f;
+	private Rigidbody2D rg2d;
+	private bool facingRight = true;
+
+	private Animator anim;
+
+	private bool ground = false;
+	public Transform groundCheck;
+	private float groundRadius = 0.2f;
+	public LayerMask grounded;
+
+	public float jumpPower = 700f;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rg2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ground && Input.GetKeyDown (KeyCode.Space))
+		{
+			anim.SetBool ("Ground", false);
+			rg2d.AddForce (new Vector2(0,jumpPower));
+		}
+	}
+
+	void FixedUpdate()
+	{
+		ground = Physics2D.OverlapCircle (groundCheck.position, groundRadius, grounded);
+
+		anim.SetBool ("Ground", ground);
+
+		anim.SetFloat ("vSpeed",rg2d.velocity.y);
+
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs(move));
+		rg2d.velocity = new Vector2 (move * topSpeed,rg2d.velocity.y);
+		if (move > 0 && !facingRight) 
+		{
+			Flip ();
+		}
+		else if(move<0 && facingRight)
+		{
+			Flip();
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
