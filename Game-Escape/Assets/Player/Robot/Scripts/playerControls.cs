d2d6a750fabc7265d0bf7139dc0cf4767f72b0e4
Krasimir <k_kostov95@abv.bg>
2017-04-09 13:29:35 +0100

    Death Reloads the scene. Score label added.

---
+++ Game-Escape/Assets/Player/Robot/Scripts/playerControls.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerControls : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody2D rg2d;
+	private bool facingRight = true;
+
+	public float speedMultiplier;
+	public float speedTarget;
+	private float speedTargetCounter;
+
+	private Animator anim;
+
+	private bool ground = false;
+	public Transform groundCheck;
+	private float groundRadius = 0.1f;
+	public LayerMask grounded;
+
+	public float jumpPower = 10f;
+	public float jumpTime;
+	private float jumpTimer;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rg2d = GetComponent<Rigidbody2D> ();
+		rg2d.isKinematic = false;
+		jumpTimer = jumpTime;
+		speedTargetCounter = speedTarget;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		ground = Physics2D.OverlapCircle (groundCheck.position, groundRadius, grounded);
+		anim.SetFloat ("vSpeed",rg2d.velocity.y);
+		anim.SetBool ("Ground", ground);
+		anim.SetFloat ("Speed", rg2d.velocity.x);
+		if (transform.position.x > speedTargetCounter) 
+		{
+			speed = speed*speedMultiplier;
+			speedTarget = speedTarget * speedMultiplier;
+			speedTargetCounter += speedTarget;
+
+		}
+
+		rg2d.velocity = new Vector2 (speed,rg2d.velocity.y);
+
+		if (ground && Input.GetKeyDown (KeyCode.Space))
+		{
+			anim.SetBool ("Ground", false);
+			rg2d.velocity = new Vector2 (rg2d.velocity.x,jumpPower);
+			//rg2d.AddForce (new Vector2(0,jumpPower));
+		}
+		if (jumpTimer>0 && Input.GetKey (KeyCode.Space))
+		{
+			rg2d.velocity = new Vector2 (rg2d.velocity.x,jumpPower);
+			//rg2d.AddForce (new Vector2(0,jumpPower));
+			jumpTimer -= Time.deltaTime;
+		}
+		if (Input.GetKeyUp (KeyCode.Space))
+		{
+			jumpTimer = 0;
+		}
+		if (ground) 
+		{
+			jumpTimer = jumpTime;
+		}
+
+
+	}
+	/*
+	//Full Control
+	void FixedUpdate()
+	{
+		ground = Physics2D.OverlapCircle (groundCheck.position, groundRadius, grounded);
+
+		anim.SetBool ("Ground", ground);
+
+		anim.SetFloat ("vSpeed",rg2d.velocity.y);
+
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs(move));
+		rg2d.velocity = new Vector2 (move * speed,rg2d.velocity.y);
+		if (move > 0 && !facingRight) 
+		{
+			Flip ();
+		}
+		else if(move<0 && facingRight)
+		{
+			Flip();
+		}
+		if (ground && Input.GetKeyDown (KeyCode.Space))
+		{
+			anim.SetBool ("Ground", false);
+			rg2d.velocity = new Vector2 (rg2d.velocity.x,jumpPower);
+			//rg2d.AddForce (new Vector2(0,jumpPower));
+		}
+		if (jumpTimer>0 && Input.GetKey (KeyCode.Space))
+		{
+			rg2d.velocity = new Vector2 (rg2d.velocity.x,jumpPower);
+			//rg2d.AddForce (new Vector2(0,jumpPower));
+			jumpTimer -= Time.deltaTime;
+		}
+		if (Input.GetKeyUp (KeyCode.Space))
+		{
+			jumpTimer = 0;
+		}
+		if (ground) 
+		{
+			jumpTimer = jumpTime;
+		}
+	}
+		
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+	*/
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log ("Reaching");
+		if (collision.gameObject.tag == "Death") 
+		{
+			Scene scene = SceneManager.GetActiveScene(); 
+			SceneManager.LoadScene(scene.name);
+		}
+	}
+}
