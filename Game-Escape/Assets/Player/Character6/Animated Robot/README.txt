1e73feed650ac2148862efe07bc3f8a79159bb53
Krasimir <k_kostov95@abv.bg>
2017-05-07 18:02:12 +0100

    Choosing a character scene has initial design

---
+++ Game-Escape/Assets/Player/Character6/Animated Robot/README.txt
@@ -0,0 +1,5 @@
+Created by Joseph Crown
+All sprites are attribution free and can be used for commercial and non-commercial purposes.
+
+Attribution is not mandatory but i would be grateful if you considered adding it.
+Opengameart.org/users/jcrown41
---
+++ Game-Escape/Assets/Scripts/CharacterTransition.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterTransition : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public GameObject nextButton;
+	public GameObject previousButton;
+	private int iteration;
+	private Image image;
+	// Use this for initialization
+	void Start () {
+		iteration = 0;
+		image = gameObject.GetComponent<Image> ();
+		image.sprite = sprites[iteration];
+	}
+
+	public void next()
+	{
+		if (iteration == 0) {
+			previousButton.GetComponent<Image> ().enabled = true;
+			previousButton.GetComponent<Button> ().enabled = true;
+		}
+		iteration++;
+		image.sprite = sprites[iteration];
+		if (iteration == sprites.Length - 1) 
+		{
+			nextButton.GetComponent<Image> ().enabled = false;
+			nextButton.GetComponent<Button> ().enabled = false;
+		}
+	}
+	public void previous()
+	{
+		if (iteration == sprites.Length - 1)
+		{
+			nextButton.GetComponent<Image> ().enabled = true;
+			nextButton.GetComponent<Button> ().enabled = true;
+		}
+		iteration--;
+		image.sprite = sprites[iteration];
+		if (iteration == 0)
+		{
+			previousButton.GetComponent<Image> ().enabled = false;
+			previousButton.GetComponent<Button> ().enabled = false;
+		}
+	}
+}
